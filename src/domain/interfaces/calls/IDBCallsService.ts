03a07699da094421bb3330dede881398f168d74b
madcativ <alextfwc@gmail.com>
2024-10-02 00:19:12 -0600

    Added dbConnService to IDBCallsService

--- src/domain/interfaces/calls/IDBCallsService.ts
+++ src/domain/interfaces/calls/IDBCallsService.ts
@@ -1,8 +1,10 @@
+import IDBConnService from "../conn/IDBConnService"
 import IDBCallsRequestQuery from "./IDBCallsRequestQuery"
 import IDBCallsRequestSP from "./IDBCallsRequestSP"
 import IDBCallsResult from "./IDBCallsResult"
 
 export default interface IDBCallsService{
+    dbConnService : IDBConnService
     CallQuery<T>(request : IDBCallsRequestQuery) : Promise<IDBCallsResult<T>>
     CallSP<T>(request : IDBCallsRequestSP) : Promise<IDBCallsResult<T>>
 }
